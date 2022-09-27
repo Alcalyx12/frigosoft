097a1ef6a8f6a78572021ef21d54f93d62d1b48b
Alcalyx12 <duvantitan@gmail.com>
2022-09-27 15:14:40 -0500

    Añadiendo proyecto

---
+++ BLL/CompraService.cs
@@ -0,0 +1,68 @@
+﻿using DAL;
+using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class CompraService
+    {
+        private ConnectionManager connectionManager;
+        private CompraRepository compraRepository;
+        IList<Compra> compras;
+
+        public CompraService(string stringConnection)
+        {
+            connectionManager = new ConnectionManager(stringConnection);
+            compraRepository = new CompraRepository(connectionManager);
+            compras = new List<Compra>();
+        }
+
+        public String GuardarCompra(Compra compra)
+        {
+            try
+            {
+                connectionManager.Open();
+                compraRepository.GuardarCompra(compra);
+                connectionManager.Close();
+                return $"Se ha registrado la compra al proveedor {compra.NombreProveedor} con exito ";
+            }
+            catch (Exception e)
+            {
+                return $"Error al guardar los datos de la compra, error  {e.Message}";
+            }
+            finally
+            {
+                connectionManager.Close();
+            }
+        }
+
+        public Compra CrearCompra()
+        {
+            Compra compra = new Compra();
+            return compra;
+        }
+
+        public IList<Compra> Consultar()
+        {
+            try
+            {
+                connectionManager.Open();
+                compras = compraRepository.Consultar();
+                connectionManager.Close();
+                return compras;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                connectionManager.Close();
+            }
+        }
+    }
+}
---
+++ DAL/ConnectionManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class ConnectionManager
+    {
+        public string ConnectionString;
+        internal SqlConnection conexion;
+
+        public ConnectionManager(string connectionString)
+        {
+            ConnectionString = connectionString;
+            conexion = new SqlConnection(connectionString);
+        }
+
+        public void Open()
+        {
+            conexion.Open();
+        }
+        public void Close()
+        {
+            conexion.Close();
+        }
+    }
+}
